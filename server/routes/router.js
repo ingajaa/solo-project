68befa0bb5fe46b1223e82644cf38d2fc4be2c17
Inga Jaanimets <ijaanimets@gmail.com>
2022-01-18 20:52:59 +0200

    fixed const

--- server/routes/router.js
+++ server/routes/router.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
     }
 });
 
-var upload = multer({
+const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
         if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
