39acfe90d91bd21e7d20bc9d68d2a4a0ff6e8804
Inga Jaanimets <ijaanimets@gmail.com>
2022-01-18 02:49:18 +0200

    Functional component for upload

--- client/src/FilesUploadComponent/FilesUploadComponent.js
+++ client/src/FilesUploadComponent/FilesUploadComponent.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './FilesUploadComponent.css'
 
-export default class FilesUploadComponent extends Component {
+export default (props) => {
+    const [profileImg, setProfileImg] = useState();
 
-    constructor(props) {
-        super(props);
-
-        this.onFileChange = this.onFileChange.bind(this);
-
-        this.state = {
-            profileImg: ''
-        }
-    }
-
-    componentDidMount() {
-                fetch('http://localhost:4000/api/').then(res => res.json()).then(data => {
+    useEffect(() => {
+        fetch('http://localhost:4000/api/').then(res => res.json()).then(data => {
             const lastUser = data.users[data.users.length - 1];
-            this.setState({ profileImg: lastUser?.profileImg });
+            setProfileImg(lastUser?.profileImg);
         });
-    }
+    }, []);
 
-    onFileChange(e) {
+    const onFileChange = (e) => {
         const profileImg = e.target.files[0];
-        this.setState({ profileImg })
+        setProfileImg(profileImg);
 
         e.preventDefault()
         const formData = new FormData()
         formData.append('profileImg', profileImg)
         axios.post("http://localhost:4000/api/user-profile", formData, {
         }).then(res => {
-            this.setState({ profileImg: res.data.userCreated.profileImg })
+            setProfileImg(res.data.userCreated.profileImg)
         })
     }
 
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <form>
-                        <div className="form-group noprint">
-                            <input type="file" onChange={this.onFileChange} />
-                        </div>
-                        <img src={this.state.profileImg} style={{maxWidth: '200px'}} alt="Something"></img>
-                    </form>
-                </div>
+    return (
+        <div className="container">
+            <div className="row">
+                <form>
+                    <div className="form-group noprint">
+                        <input type="file" onChange={onFileChange} />
+                    </div>
+                    {profileImg ?
+                        <img className="image-to-print" src={profileImg} alt="Something"></img>
+                        : null}
+                </form>
             </div>
-        )
-    }
+        </div>
+    )
 }
